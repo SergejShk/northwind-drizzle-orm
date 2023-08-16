83892d516070c75d10c51e2c29b7b3da0a809a56
SergejShk <sshkurupij11@gmail.com>
2023-08-16 19:57:03 +0300

    fix deployment

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
+import dotenv from "dotenv";
+import postgres from "postgres";
 
-import suppliers from "./controllers/suppliers";
+dotenv.config();
+
+// import suppliers from "./controllers/suppliers";
+const dbUrl = process.env.DB_URL || ''
 
 import { errorHandler } from "./utils/errorHandlers";
 
@@ -14,7 +19,23 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use(suppliers.path, suppliers.router);
+// app.use(suppliers.path, suppliers.router);
+
+const queryClient = postgres(dbUrl, { 
+  ssl: {
+      rejectUnauthorized: false,
+  },
+});
+
+const getDbData = async () => {
+  return await queryClient`select * from suppliers`
+};
+
+app.get('/', async (_, res) => {
+  const response = await getDbData()
+
+  res.send(response)
+})
 
 app.use((_, res) => {
   res.status(404).json({ message: "Not Found" });
