11f84058c4c1a3798b3146cb6e68aa1034132774
SergejShk <sshkurupij11@gmail.com>
2023-08-16 17:56:35 +0300

    fix deployment

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,15 @@
 import "reflect-metadata";
 import { migrate } from 'drizzle-orm/postgres-js/migrator';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from "postgres";
 import dotenv from "dotenv";
 
 import { app } from "./app";
+import { migrationDb } from "./db/dbSource";
  
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const dbUrl = process.env.DB_URL || ''
 
 const serverStart = async () => {
-  const migrationClient = postgres(dbUrl, { max: 1 });
-  const migrationDb = drizzle(migrationClient);
-
   try {
     await migrate(migrationDb, { migrationsFolder: './drizzle' });
     
