ec36b4beea0eea37e21e0475fcb9328e49542dae
SergejShk <sshkurupij11@gmail.com>
2023-08-16 19:41:51 +0300

    fix deployment

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
+import dotenv from "dotenv";
+import postgres from "postgres";
+
+dotenv.config();
 
 // import suppliers from "./controllers/suppliers";
-
-import { queryClient } from "./db/dbSource";
+const dbUrl = process.env.DB_URL || ''
 
 import { errorHandler } from "./utils/errorHandlers";
 
@@ -17,6 +20,12 @@ app.use(cors());
 app.use(express.json());
 
 // app.use(suppliers.path, suppliers.router);
+
+const queryClient = postgres(dbUrl, { 
+  ssl: {
+      rejectUnauthorized: false,
+  },
+});
 
 const getDbData = async () => {
   return await queryClient`select * from suppliers`
