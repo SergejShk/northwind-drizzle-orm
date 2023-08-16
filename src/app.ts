e876a21e4d2ca850590f3ec6b96fed5e266acf87
SergejShk <sshkurupij11@gmail.com>
2023-08-16 17:26:38 +0300

    Upd imports

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 
-import suppliers from "controllers/suppliers";
+import suppliers from "./controllers/suppliers";
 
 import { errorHandler } from "./utils/errorHandlers";
 
